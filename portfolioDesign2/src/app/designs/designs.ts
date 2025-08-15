13a42c29c7f13fca82214ea1f916942c4a7bf680
Lucas Setiady <luca.setia@gmail.com>
2025-08-15 13:40:56 +1000

    Components: Generated all components for Portfolio Design

---
+++ portfolioDesign2/src/app/designs/designs.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-designs',
+  imports: [],
+  templateUrl: './designs.html',
+  styleUrl: './designs.css'
+})
+export class Designs {
+
+}
